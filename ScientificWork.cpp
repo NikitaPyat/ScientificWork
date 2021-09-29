c2d82bb8129e7291f26cc2c1eb70e933cc3fd263
NikitaPyat <73129357+NikitaPyat@users.noreply.github.com>
2021-09-29 23:54:05 +0300

    Add files via upload

--- ScientificWork.cpp
+++ ScientificWork.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 
-struct Specifications{
+struct Specifications {
+    int E0;
     int E1;
     int E2;
     int w;
     int mu;
     int h;
     int Z0;
+    int lambda;
+    int omega;
 };
+
+Specifications sp = { 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
+
+int eta0 = sqrt(sp.lambda ^ 2 - sp.omega ^ 2 * sp.E0 * sp.mu);
+int eta1 = sqrt(sp.lambda ^ 2 - sp.omega ^ 2 * sp.E1 * sp.mu);
+int eta2 = sqrt(sp.lambda ^ 2 - sp.omega ^ 2 * sp.E2 * sp.mu);
+
+double eta01() {
+    return (eta0 - eta1) / (eta0 + eta1);
+}
+
+double eta12() {
+    return (eta1 - eta2) / (eta1 + eta2);
+}
+
 int main() {
-    
+    int R1 = eta01() * exp(-2 * eta1 * sp.h);
+    int R0 = (R1 + eta01()) / (R1 * eta01() + 1);
 }
